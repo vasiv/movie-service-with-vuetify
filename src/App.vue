5dbba967e14fbc9facce9fbf6b98fb34022293fe
Szymon Ciepluch <szymon.ciepluch95@gmail.com>
2021-01-04 13:45:59 +0100

    homepage sketch implemented

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,73 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
     <v-app-bar
       app
-      color="primary"
-      dark
+      shrink-on-scroll
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-container>
+      <v-img
+      src="./assets/logo.png"
+      class="mx-auto"
+      contain
+      max-height="60"
+      max-width="60"
+      ></v-img>
+    </v-container>
+
+    <v-container>
+      <v-form>
+              <v-text-field
+                  label="Szukaj"
+                  placeholder="Szukaj"
+              ></v-text-field>
+            </v-form>
+      </v-container>
+
+    <v-container>
+      <v-btn rounded>
+        Baza filmów
       </v-btn>
-    </v-app-bar>
+    </v-container>
 
-    <v-main>
-      <HelloWorld/>
+      <v-btn depressed>
+        Zaloguj się
+      </v-btn>
+  </v-app-bar>
+
+  <v-main>
+  <v-container>
+        <v-row>
+          <v-col
+            v-for="n in 9"
+            :key="n"
+            cols="4"
+          >
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
+
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>IUI - Serwis Filmowy</strong>
+    </v-col>
+  </v-footer>
+
   </v-app>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
+  export default {
     //
-  }),
-};
+  }
 </script>
